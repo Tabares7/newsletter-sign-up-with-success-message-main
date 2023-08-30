a142dedb9c0dac574e468a395c43e4cad5cf2f82
tabares7 <pedrotabares777@gmail.com>
2023-08-30 13:46:00 -0500

    All responsive

--- signinglist/src/assets/components/Layout.jsx
+++ signinglist/src/assets/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className='mx-auto md:bg-paleblack md:p-32 '>
 
-        <div className="flex flex-col  gap-3  md:flex-row-reverse md:bg-white justify-center items-center md:p-8 ">
+        <div className=" max-w-max md:rounded-3xl flex flex-col  gap-3  md:flex-row-reverse md:bg-white mx-auto items-center md:p-8 ">
         <SVGSwitch/>
        <Content/>
         </div>
